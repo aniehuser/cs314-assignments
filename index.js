aa00a102901e5d93ce75ce390bc1263941eaabb1
Anthony Niehuser <tniehuser@gmail.com>
2018-04-23 15:07:29 -0700

    turn in

--- index.js
+++ index.js
@@ -1,75 +1,119 @@
 "use strict";
 
+let apiRoot = 'https://jsonplaceholder.typicode.com';
 
-///SElection sectino
-// could make dynamic
-let sectionStart = [150,300,475,668,882];
 
-let scrollButtons = document.body.querySelectorAll("#selection > div > button");
+$(document).ready(()=>{
+	fetch(apiRoot + '/users')
+	.then(response => response.json())
+	.then(json => addUsers(json))
+})
 
-for(let i=0; i<5; i++){
-	scrollButtons[i].addEventListener("click", (event) => { 
-		window.scrollTo(0, sectionStart[i]); 
-		console.log("scroll to " + i)
-	})
+
+
+let addUsers = function(json) {
+	console.log(json.length);
+	for(let i=0; i<json.length; i++){
+		let but1 = document.createElement("button")
+		but1.innerHTML = "Todos";
+		but1.setAttribute("class", "click-button")
+		let but2 = document.createElement("button")
+		but2.innerHTML = "Albums";
+		but2.setAttribute("class", "click-button")
+		let div = document.createElement("div")
+		addToH(div, json[i].name, 3);
+		addToP(div, `E-mail: ${json[i].email}`);
+		addToP(div, `Company: ${json[i].company.name}`);
+		div.appendChild(but1);
+		div.appendChild(but2);
+		div.setAttribute("class", "inner-content");
+		let section = document.createElement("section")
+		section.appendChild(div)
+		section.setAttribute("id", `sec${i}`)
+		section.setAttribute("class",(i%2==0) ? "dark-bg": "");
+		document.body.appendChild(section);
+		$(but1).click(()=>{
+			let div1 = $(`#albums-${i}`)
+			let div2 = $(`#todo-${i}`)
+			if(div2.length == 0){
+				fetch(apiRoot + `/todos?userId=${i+1}`)
+					.then(response => response.json())
+					.then(json => addTodos(json));
+			} else {
+				div2.toggle();
+				div1.hide();
+			}
+		})
+		$(but2).click(()=>{
+			let div1 = $(`#albums-${i}`)
+			let div2 = $(`#todo-${i}`)
+			if(div1.length == 0){
+				fetch(apiRoot + `/albums?userId=${i+1}`)
+					.then(response => response.json())
+					.then(json => addAlbums(json))
+			} else {
+				div2.hide();
+				div1.toggle();
+			}
+
+		})
+
+	}
+}
+
+let addTodos = function(json) {
+	console.log(json);
+	let id = json[0].userId -1;
+	let bigDaddyDiv = document.createElement("div")
+	bigDaddyDiv.setAttribute("id",`todo-${id}`)
+	for(let i=0; i<json.length; i++){
+		let div = document.createElement("div");
+		let span = document.createElement("span");
+		span.setAttribute("class", "flex")
+		addToH(div, `Todo #${i+1}`, 4);
+		addToP(span, json[i].title)
+		let icon = document.createElement("i");
+		console.log(typeof(json[i].completed))
+		icon.setAttribute("class", (json[i].completed) ? "fas fa-check-square green":"fas fa-times-circle red")
+		icon.classList.add("pad-1")
+		span.append(icon);
+		div.append(span);
+		$(bigDaddyDiv).append(div);
+	}
+	$(`#albums-${id}`).hide()
+	$(`#sec${id}`).append(bigDaddyDiv);
+}
+
+let addAlbums = function(json) {
+	console.log(json);
+	let id = json[0].userId -1;
+	let bigDaddyDiv = document.createElement("div")
+	bigDaddyDiv.setAttribute("id",`albums-${id}`)
+	for(let i=0; i<json.length; i++){
+		let div = document.createElement("div");
+		addToH(div, `Album #${i+1}`, 4);
+		addToP(div, json[i].title)
+		$(bigDaddyDiv).append(div);
+	}
+	$(`#todo-${id}`).hide();
+	$(`#sec${id}`).append(bigDaddyDiv);
+
 }
 
 
-let section1 = document.querySelector("#one");
-let section2 = document.querySelector("#two");
-let section3 = document.querySelector("#three");
-let section4 = document.querySelector("#four");
-let section5 = document.querySelector("#five");
-
-// Sectoin 1
-let colorButtons = section1.querySelectorAll("button");
-colorButtons[0].addEventListener("click", (event) => {
-	section1.style.backgroundColor = "blue";
-	console.log("blue")
-})
-
-colorButtons[1].addEventListener("click", (event) => {
-	section1.style.backgroundColor = "green";
-	console.log("green")
-})
-
-// Section 2
-let toggleColorButton = section2.querySelector("button");
-toggleColorButton.addEventListener("click", (event) => {
-	section2.style.backgroundColor = (section2.style.backgroundColor == "pink") ? "orange" : "pink";
-	console.log("pink/orange");
-})
-
-// section3
-let addToListButton = section3.querySelector("button");
-addToListButton.addEventListener("click", (event) => {
-	let table = section3.querySelector("ul");
-	let form = section3.querySelector("input");
-	let newEle = document.createElement("li");
-	let node = document.createTextNode(form.value);
-	newEle.appendChild(node);
-	table.appendChild(newEle);
-})
-
-// section 4
-let removeClicker = section4.querySelectorAll("li");
-for(let i=0; i<4; i++){
-	removeClicker[i].addEventListener("click", (event) => {
-		event.target.parentNode.removeChild(event.target);
-		console.log("remove " + i);
-	})	
+let addToH = function(ele, innerhtml, h=1){
+	let newH = document.createElement(`h${h}`);
+	newH.innerHTML = innerhtml;
+	ele.appendChild(newH);
 }
 
-// section 5
-let highlightClicker = section5.querySelectorAll("li");
-for(let i=0; i<4; i++) {
-	highlightClicker[i].addEventListener("click", (event) => {
-		for(i=0; i<4; i++){
-			highlightClicker[i].classList.remove("highlight");
-		}
-		event.target.classList.add("highlight");
-	})
+let addToP = function(ele, innerhtml){
+	let p = document.createElement("p")
+	p.innerHTML = innerhtml;
+	ele.appendChild(p);
 }
 
 
+
+// console.log(users);
 
